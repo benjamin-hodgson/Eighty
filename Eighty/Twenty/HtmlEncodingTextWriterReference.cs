51b6d310031edebadd2d3bb4fade720153847ee2
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-03-13 23:28:33 +0000

    Use IntPtr not UIntPtr cos it's more compatible

--- Eighty/Twenty/HtmlEncodingTextWriterReference.cs
+++ Eighty/Twenty/HtmlEncodingTextWriterReference.cs
@@ -24,6 +24,6 @@ namespace Eighty.Twenty
             return ref Unsafe.AsRef<HtmlEncodingTextWriter>(_writer);
         }
 
-        public bool IsDefault() => new UIntPtr(_writer).Equals(UIntPtr.Zero);
+        public bool IsDefault() => new IntPtr(_writer).Equals(IntPtr.Zero);
     }
 }
