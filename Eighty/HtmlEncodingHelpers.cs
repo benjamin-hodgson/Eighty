903a32064ad456332252a827f6de2c9e114fda34
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-03-26 16:47:48 +0100

    Remove unused code

--- Eighty/HtmlEncodingHelpers.cs
+++ Eighty/HtmlEncodingHelpers.cs
@@ -6,9 +6,6 @@ namespace Eighty
     {
         public const string UNICODE_REPLACEMENT_CHAR = "\uFFFD";
 
-        public static bool IsBasicMultilingualPlane(int codePoint)
-            => codePoint <= 0x00FFFF;
-
         public static unsafe int FindFirstCharacterToEncode(this HtmlEncoder htmlEncoder, string s, int start)
         {
             fixed (char* t = s)
@@ -16,9 +13,5 @@ namespace Eighty
                 return htmlEncoder.FindFirstCharacterToEncode(t + start, s.Length - start);
             }
         }
-
-        public static bool ShouldEncode(char c)
-            => (c <= '>' && (c == '<' || c == '>' || c == '&' || c == '"' || c == '\''))
-            || char.IsSurrogate(c);
     }
 }
