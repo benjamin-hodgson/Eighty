ed99d369cf3064baa3389c6d6a6e8f5eb6530b56
Benjamin Hodgson <bhodgson@stackoverflow.com>
2018-10-15 23:31:26 -0400

    update BenchmarkDotNet

--- Eighty.Bench/SimpleBench.cs
+++ Eighty.Bench/SimpleBench.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Engines;
 using Eighty.Twenty;
--- Eighty.Bench/WebAppBench.cs
+++ Eighty.Bench/WebAppBench.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Jobs;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 
